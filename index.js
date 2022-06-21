54581ac4b27877998e7123047ad10f202d6a8120
khoivc <khoi.vu2@sotatek.com>
2022-06-21 18:03:36 +0700

    aaaa

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const args = process.argv
-  .filter((item) => item.includes("--"))
-  .reduce((acc, cur) => {
-    const [key, value] = cur.split("=");
-    return {
-      ...acc,
-      [key.replace("--", "")]: value,
-    };
-  }, {});
+const args = process.argv;
+//   .filter((item) => item.includes("--"))
+//   .reduce((acc, cur) => {
+//     const [key, value] = cur.split("=");
+//     return {
+//       ...acc,
+//       [key.replace("--", "")]: value,
+//     };
+//   }, {});
 
 console.log("args", args);
+
+import { GoogleChatWebhook } from "google-chat-webhook";
+const url = process.env.WEBHOOK_URL;
+if (!url) {
+  throw new Error("Environment variable 'WEBHOOK_URL' must be set.");
+}
+const client = new GoogleChatWebhook({ url });
