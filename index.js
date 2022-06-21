29c7ab1f9dcd6c08674b474bf34c47711aee7a13
khoivc <khoi.vu2@sotatek.com>
2022-06-21 21:47:01 +0700

    ahhaha

--- index.js
+++ index.js
@@ -9,11 +9,9 @@ const {
   PR_NUMBER,
 } = process.env;
 
-console.log("aaaaaaaaaaaPR_NUMBER", INPUT_EVENTNUMBER);
-
-// import { GoogleChatWebhook } from "google-chat-webhook";
-// const url = process.env.WEBHOOK_URL;
-// if (!url) {
-//   throw new Error("Environment variable 'WEBHOOK_URL' must be set.");
-// }
-// const client = new GoogleChatWebhook({ url });
+import { GoogleChatWebhook } from "google-chat-webhook";
+const url = process.env.INPUT_WEBHOOK;
+if (!url) {
+  throw new Error("Environment variable 'WEBHOOK_URL' must be set.");
+}
+const client = new GoogleChatWebhook({ url });
